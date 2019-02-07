c02ab9d016b09b3fe18e85b7eb8aae1d668bf274
Paul Melnikov <positron96@gmail.com>
2019-02-07 15:55:29 +0700

    disabled EXTENDED_CAPABILITIES_REPORT. 114,666 bytes (88%)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1440,7 +1440,7 @@
 /**
  * Include capabilities in M115 output
  */
-#define EXTENDED_CAPABILITIES_REPORT
+//#define EXTENDED_CAPABILITIES_REPORT
 
 /**
  * Disable all Volumetric extrusion options
